97bd884b1ffc3c694d4b2072c5e530d1520619d0
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2018-08-20 14:03:27 -0400

    Added a unittest for inplace-add.

--- tests/test_bleu.py
+++ tests/test_bleu.py
@@ -6,6 +6,8 @@ import unittest
 import bleu
 from bleu import bleuStats
 
+
+
 class TestBleu(unittest.TestCase):
     def testWikipedia(self):
         candidate = 'the the the the the the the'
@@ -27,3 +29,21 @@ class TestBleu(unittest.TestCase):
         self.assertAlmostEqual(b.score(bleu.smooth_2), -1.64997, 4)
         self.assertAlmostEqual(b.score(bleu.smooth_3), -1.00634, 4)
         self.assertAlmostEqual(b.score(bleu.smooth_4), -2.54978, 4)
+
+
+    def testInplaceAdd(self):
+        candidate1 = 'the the the the the the the'
+        candidate2 = 'the the cat'
+        ref1 = 'the cat is on the mat'
+        ref2 = 'there is a cat on the mat'
+
+        b1 = bleuStats(candidate1, [ref1, ref2])
+        b2 = bleuStats(candidate2, [ref1, ref2])
+
+        b1 += b2
+
+        self.assertEqual(b1.len, 10)
+        self.assertEqual(b1.bmlen, 13)
+        self.assertSequenceEqual(b1.match, (5.0, 1.0, 0.0, 0.0))
+        self.assertSequenceEqual(b1.total, (10, 8, 6 ,4))
+        self.assertAlmostEqual(b1.brevity_penalty, -0.3, 4)
